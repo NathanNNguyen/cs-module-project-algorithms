3342dc7f8c28d56974a9854165a6d04ede3f66c6
Nhan <natenguyen51294@gmail.com>
2020-07-22 14:39:49 -0500

    fixed single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,9 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 
-# this method would work if we're given that specific array
 
 def single_number(arr):
+
     # check for length of arr
     if len(arr) == 0:
         return
@@ -13,14 +13,24 @@ def single_number(arr):
     elif len(arr) == 1:
         return arr[0]
 
-    i = 0
-
-    while i in range(len(arr)):
-        if arr[i] != arr[i + 1]:
+    for i in range(len(arr)):
+        # using the count method to count
+        # the number of element appear
+        # in a given list
+        if arr.count(arr[i]) == 1:
             return arr[i]
-        i += 2
 
     return arr[i]
+
+    # this method would work if we're given that specific array
+    # i = 0
+
+    # while i in range(len(arr)):
+    #     if arr[i] != arr[i + 1]:
+    #         return arr[i]
+    #     i += 2
+
+    # return arr[i]
 
 
 if __name__ == '__main__':
